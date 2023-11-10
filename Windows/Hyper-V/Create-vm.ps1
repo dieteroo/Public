42b7de340ae42c06bb5969ad31917c82d0e68a1b
dieteroo <web@oosterbaan.be>
2023-11-10 12:41:58 +0100

    v1.4
    Added network switch type choice

--- Windows/Hyper-V/Create-vm.ps1
+++ Windows/Hyper-V/Create-vm.ps1
@@ -1,12 +1,13 @@
  #--------------------------------------------------------------------------#
 #- Created by:             Dieter Oosterbaan                              -#
-#- Version:                1.3                                            -#
+#- Version:                1.4                                            -#
 #--------------------------------------------------------------------------#
 # Change Log                                                              -#
 # 25 October 2023          Initial Script for Windows Server 2022         -#
 # 10 November 2023         Added workstation or server choice             -#
 # 10 November 2023         Added second HDD for server                    -#
 # 10 November 2023         Computername prompt name standardization       -#
+# 10 November 2023         Added network switch type choice               -#
 #--------------------------------------------------------------------------#
 
 #-------------#
@@ -14,9 +15,8 @@
 #-------------#
 
 $ParentDir = 'C:\Virtual_Machines'
-$Switch = 'ExternalVirtualSwitch'
 
- #---------------------------#
+#---------------------------#
 #- Prompt for computername -#
 #---------------------------#
 Write-Host "VM-WIN-S- or VM-WIN-W- will be added automagically" -ForegroundColor Yellow
@@ -67,14 +67,38 @@ if (Test-Path $ParentDir\$VMName) {
 	break
 }
 
-#-----------------------------------#
-#- Create virtual switch if needed -#
-#-----------------------------------#
+#---------------------------------------------------------------#
+#- Create -- private, internal or external switch -- if needed -#  
+#---------------------------------------------------------------#
+$SwitchTypeChoice = [System.Management.Automation.Host.ChoiceDescription[]](@(
+    (New-Object System.Management.Automation.Host.ChoiceDescription("&Private", "The VMs connected to a private virtual switch can communicate with each other on the same Hyper-V server but not with the external network.")),
+    (New-Object System.Management.Automation.Host.ChoiceDescription("&External", "The VMs connected to an external virtual switch can fully communicate with each other, with the host, with the external network, and with the Internet.")),
+    (New-Object System.Management.Automation.Host.ChoiceDescription("&Internal", "The VMs connected to an internal virtual switch can communicate with each other and with the host operating system."))
+))
+$SwitchType = $Host.Ui.PromptForChoice("Environment", "Choose the target environment", $SwitchTypeChoice, 0)
+
 $ethernet = Get-NetAdapter -Name "Ethernet 5"
 
-If ( ! ( Get-VMSwitch | Where {$_.Name -eq $Switch} ) ) {
-    New-VMSwitch -Name $Switch -NetAdapterName $ethernet.Name -AllowManagementOS $True
+if($SwitchType -eq 0) {
+$Switch = 'PrivateVirtualSwitchtest'    
+    If ( ! ( Get-VMSwitch | Where {$_.Name -eq $Switch} ) ) {
+        New-VMSwitch -Name $Switch -SwitchType Private
+    }
 }
+
+if($SwitchType -eq 1) {
+$Switch = 'ExternalVirtualSwitchtest'
+    If ( ! ( Get-VMSwitch | Where {$_.Name -eq $Switch} ) ) {
+        New-VMSwitch -Name $Switch -NetAdapterName $ethernet.Name -AllowManagementOS $True
+    }
+}
+
+if($SwitchType -eq 2) {
+$Switch = 'InternalVirtualSwitchtest'    
+    If ( ! ( Get-VMSwitch | Where {$_.Name -eq $Switch} ) ) {
+        New-VMSwitch -Name $Switch -SwitchType Internal
+    }
+} 
 
 #----------------------------#
 #- create a virtual machine -#
