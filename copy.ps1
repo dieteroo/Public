80c3c49b19bbf550af8feb933d8412f03bb169bb
dieteroo <web@oosterbaan.be>
2023-11-13 13:22:36 +0100

    Update copy.ps1

--- copy.ps1
+++ copy.ps1
@@ -3,27 +3,26 @@
 
 [System.Collections.ArrayList] $filesToMove = Get-ChildItem -Path $SourceDirectoryPath -File -Force -Recurse
 
-$filesToMove | ForEach-Object {
-    [System.IO.FileInfo] $file = $_
+if ($filesToMove.count -gt 0) {
+    $filesToMove | ForEach-Object {
+        [System.IO.FileInfo] $file = $_
 
-    [DateTime] $fileDate = $file.LastWriteTime
-    [string] $dateDirectoryName = $fileDate.ToString('yyyy-MM-dd')
-    [string] $dateDirectoryPath = Join-Path -Path $TargetDirectoryPath -ChildPath $dateDirectoryName
+        [DateTime] $fileDate = $file.LastWriteTime
+        [string] $dateDirectoryName = $fileDate.ToString('yyyy-MM-dd')
+        [string] $dateDirectoryPath = Join-Path -Path $TargetDirectoryPath -ChildPath $dateDirectoryName
 
-    if (!(Test-Path -Path $dateDirectoryPath -PathType Container))
-    {
-        Write-Host "Aanmaken van folder '$dateDirectoryPath'." -ForegroundColor Yellow
-        New-Item -Path $dateDirectoryPath -ItemType Directory -Force > $null
-    }
+        if (!(Test-Path -Path $dateDirectoryPath -PathType Container)) {
+            Write-Host "Aanmaken van folder '$dateDirectoryPath'." -ForegroundColor Yellow
+            New-Item -Path $dateDirectoryPath -ItemType Directory -Force > $null
+        }
 
-    [string] $filePath = $file.FullName
-    if (!(Test-Path -Path $dateDirectoryPath/$file -PathType Leaf))
-    {
-        Write-Host "Verplaatsen van '$filePath' naar folder '$dateDirectoryPath' gelukt" -ForegroundColor Green
-        Move-Item -Path $filePath -Destination $dateDirectoryPath
-    }
-    else
-    {
-    Write-Host "Bestand '$filePath' bestaat reeds in folder '$dateDirectoryPath'." -ForegroundColor Red
+        [string] $filePath = $file.FullName
+        if (!(Test-Path -Path $dateDirectoryPath/$file -PathType Leaf)) {
+            Write-Host "Verplaatsen van '$filePath' naar folder '$dateDirectoryPath' gelukt" -ForegroundColor Green
+            Move-Item -Path $filePath -Destination $dateDirectoryPath
+        }
+        else {
+            Write-Host "Bestand '$filePath' bestaat reeds in folder '$dateDirectoryPath'." -ForegroundColor Red
+        }
     }
 }
