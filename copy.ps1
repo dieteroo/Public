5fc63fe7b048fc4ba0c315f03245d7fca6bb69b7
dieteroo <web@oosterbaan.be>
2023-11-13 12:33:10 +0100

    Create copy.ps1

---
+++ copy.ps1
@@ -0,0 +1,22 @@
+[string] $SourceDirectoryPath = 'C:\Users\test\DCIM'
+[string] $TargetDirectoryPath = 'C:\Users\test\Te sorteren'
+
+[System.Collections.ArrayList] $filesToMove = Get-ChildItem -Path $SourceDirectoryPath -File -Force -Recurse
+
+$filesToMove | ForEach-Object {
+    [System.IO.FileInfo] $file = $_
+
+    [DateTime] $fileDate = $file.LastWriteTime
+    [string] $dateDirectoryName = $fileDate.ToString('yyyy.MM.dd')
+    [string] $dateDirectoryPath = Join-Path -Path $TargetDirectoryPath -ChildPath $dateDirectoryName
+
+    if (!(Test-Path -Path $dateDirectoryPath -PathType Container))
+    {
+        Write-Verbose "Creating directory '$dateDirectoryPath'."
+        New-Item -Path $dateDirectoryPath-ItemType Directory -Force > $null
+    }
+
+    [string] $filePath = $file.FullName
+    Write-Information "Moving file '$filePath' into directory '$dateDirectoryPath'."
+    Move-Item -Path $filePath -Destination $dateDirectoryPath
+}
